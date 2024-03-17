e4599071393b3e6109ad050e730cad5d8d40d2a1
laveshgaurav <lavesh.g96@gmail.com>
2024-03-17 13:50:46 +0530

    inventory mapping

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
-const data = require("./data.json");
+const { default: mongoose } = require("mongoose");
+const InventoryModel = require("./models/inventoryModel");
 
 app.use(cors());
 app.use(
@@ -13,86 +14,32 @@ app.use(
 app.use(express.json());
 app.use(morgan("tiny"));
 
-app.post("/:id/:name", (req, res) => {
-  console.log("PARAMS", req.params); //Pre defined
-  console.log("QUERY", req.query); // Embedded in request url
-  console.log("BODY", req.body);
-  return res.send({
-    status: true,
-    data: {
-      params: req.params,
-      query: req.query,
-      body: req.body,
-    },
-  });
-});
-
-app.get("/get-user", (req, res) => {
-  const userData = data;
-  return res.send({
-    data: userData,
-    status: true,
-  });
-});
-
-app.get("/get-user-by-id/:id", (req, res) => {
-  let id = Number(req.params.id);
-  let userFound = data.find((d) => d.id === id);
-  if (!userFound) {
-    return res.send({
-      data: "User not found!",
-      status: false,
-    });
-  }
-  return res.send({
-    data: userFound,
-    status: true,
-  });
-});
-
-app.delete("/delete-user-by-id/:id", (req, res) => {
-  let id = Number(req.params.id);
-  let userFound = data.find((d) => d.id === id);
-  if (!userFound) {
-    return res.send({
-      data: "User not found!",
-      status: false,
-    });
-  }
-  let updatedUser = data.filter((d) => d.id !== id);
-  return res.send({
-    data: updatedUser,
-    status: true,
-  });
-});
-
-app.post("/create-user", (req, res) => {
-  let id = req.body.id;
-  if (!id) {
-    return res.send({
-      data: "User id is missing.",
-      status: false,
-    });
-  }
-  let userFound = data.find((d) => d.id === id);
-  if (userFound) {
-    return res.send({
-      data: "User id exists",
-      status: false,
-    });
-  }
-
-  let newData = [...data, { ...req.body }];
-
-  return res.send({
-    data: newData,
-    status: true,
-  });
-});
-
-app.post("/update-user-by-id/:id");
+// app.post("/create-inventory", async (req, res) => {
+//   try {
+//     let data = req.body.data;
+//     let resp = await InventoryModel.insertMany(data);
+//     console.log("Resp", resp);
+//     return res.send({
+//       message: "Data Inserted",
+//       status: true,
+//       resp,
+//     });
+//   } catch (e) {
+//     console.log("Errror", e);
+//     return res.send({
+//       message: e,
+//       status: false,
+//     });
+//   }
+// });
 
 app.listen(8000, () => {
   console.clear();
   console.log("Server Running on 8000..");
+  mongoose
+    .connect(
+      "mongodb+srv://webmonk:webmonk@cluster0.gp38f.mongodb.net/ecommerce?retryWrites=true&w=majority"
+    )
+    .then(() => console.log("DB Connected"))
+    .catch((e) => console.log(e));
 });
---
+++ models/inventoryModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const inventorySchema = new mongoose.Schema({
+  title: {
+    type: String,
+  },
+  price: {
+    type: Number,
+  },
+  description: {
+    type: String,
+  },
+  category: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  rating: {
+    rate: {
+      type: Number,
+    },
+    count: {
+      type: Number,
+    },
+  },
+});
+
+module.exports = mongoose.model("inventories", inventorySchema);
